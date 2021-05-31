b769ea2a4ff28464c55d5853c2b90a484264c0b0
Woosang <77317312+yws1502@users.noreply.github.com>
2021-06-01 00:36:48 +0900

    Add files via upload

---
+++ web_programming/java_script/Dream_coding/1.async와 defer의 차이점/main.js
@@ -0,0 +1,12 @@
+// Whole-script strict model syntax
+// JavaScript is very flexible
+// flexible === dangerous
+// added ECMAScript 5
+// 'use strict';
+// ==> 기존에 있던 변수 및 등등을 변경하는 일을 방지하기 위해
+'use strict';
+
+console.log('Hello World!')
+
+let a;
+a = 6;
